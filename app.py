bfa9a5d6b1f4146c5889f4d356e85b494e344bea
Parth Ghumade <Parthghumade5@gmail.com>
2026-01-30 00:56:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+import subprocess
+import os
+import uuid
+import glob
+
+app = FastAPI()
+
+@app.get("/url")
+def get_url(url: str,name: str):
+
+    file_id=str(uuid.uuid4())
+    file_path=f"downloads/{file_id}.m4a"
+
+    result=subprocess.run(
+    ["./yt-dlp", "--extractor-args","youtube:player_client=web_embedded","-f","bestaudio[ext=m4a]","-o", file_path, "--print","after_move:filepath", url],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+
+    return FileResponse(
+        path=file_path,
+        media_type="audio/mp4",
+        filename=f"{name}.m4a"
+    )
