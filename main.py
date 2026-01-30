656b10b57af3cb2b027815d4979529fed6a29522
Parth Ghumade <Parthghumade5@gmail.com>
2026-01-30 15:52:34 +0530

    Implement error handling in get_url function
    Added error handling for subprocess execution in get_url.

--- main.py
+++ main.py
@@ -17,15 +17,21 @@ def get_url(url: str,name: str):
     file_path=f"downloads/{file_id}.m4a"
 
     result=subprocess.run(
-    ["yt-dlp", "--extractor-args","youtube:player_client=web_embedded","-f","bestaudio[ext=m4a]","-o", file_path, "--print","after_move:filepath", url],
+    ["yt-dlp", "--extractor-args","youtube:player_client=web_embedded","-f","bestaudio/bestaudio[ext=m4a]","-o", file_path, "--print","after_move:filepath", url],
         capture_output=True,
-        text=True,
-        check=True
+        text=True
     )
+    
+    if result.returncode != 0:
+        raise HTTPException(
+            status_code=400,
+            detail=result.stderr
+        )
 
     return FileResponse(
         path=file_path,
         media_type="audio/mp4",
         filename=f"{name}.m4a"
     )
+
 
