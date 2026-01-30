9e83d40f6d37c22e91408cb4faf3b468f48306af
Parth Ghumade <Parthghumade5@gmail.com>
2026-01-30 16:56:08 +0530

    Implement clear_downloads function in main.py
    Added a function to clear the downloads folder before downloading a new file.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 import subprocess
 import uuid
-
+import os
 
 app = FastAPI()
 
@@ -10,28 +10,34 @@ app = FastAPI()
 def check_heath():
     return {"status":"ok"}
 
+def clear_downloads():
+    folder = "downloads"
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}: {e}")
+
+
 @app.get("/url")
 def get_url(url: str,name: str):
 
+    clear_downloads()
     file_id=str(uuid.uuid4())
     file_path=f"downloads/{file_id}.m4a"
 
     result=subprocess.run(
-    ["yt-dlp", "--extractor-args","youtube:player_client=web_embedded","-f","bestaudio/bestaudio[ext=m4a]","-o", file_path, "--print","after_move:filepath", url],
+    ["yt-dlp", "--extractor-args","youtube:player_client=web_embedded","-f","bestaudio","--extract-audio","--audio-format","m4a","--no-playlist","-o", file_path, url],
         capture_output=True,
-        text=True
+        text=True,
+        check=True
     )
-    
-    if result.returncode != 0:
-        raise HTTPException(
-            status_code=400,
-            detail=result.stderr
-        )
 
     return FileResponse(
         path=file_path,
         media_type="audio/mp4",
         filename=f"{name}.m4a"
     )
-
 
